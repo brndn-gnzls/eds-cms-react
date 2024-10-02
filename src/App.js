4e79d5e443b063b68a4c14e34988fe585a4a3b71
wrvthlss <wrgh.sushi333@passmail.net>
2024-10-02 13:53:38 -0400

    Pulling articles from Strapi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [articles, setArticles] = useState([]);
+
+    // runs when the component loads, making a GET request to Strapi's
+    // /api/articles endpoint.
+    useEffect(() => {
+        axios.get('http://localhost:1337/api/articles')
+            .then(response => {
+                // articles are stored in the articles state using setArticles.
+                setArticles(response.data.data)
+            })
+            .catch(error => {
+                console.error('[-] Error fetching articles from Strapi: ', error);
+            });
+    }, []);
+
+    // articles are mapped over to display the title and content from the
+    // attributes of each article.
+    return (
+        <div>
+            <h1>Strapi React Configuration</h1>
+            <ul>
+                {articles.map(article => (
+                    <li key={article.id}>
+                        <h2>{article.Title}</h2>
+                        {article.Content.map((contentBlock, index) => (
+                            contentBlock.children.map((child, childIndex) => (
+                                <p key={`${index}-${childIndex}`}>{child.text}</p>
+                            ))
+                        ))}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default App;
